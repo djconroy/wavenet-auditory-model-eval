aa97cd5ad3a2e807408e975ce10e40d2ae296696
Daniel Conroy <daniel.conroy@ucdconnect.ie>
2024-02-16 03:43:17 +0000

    modify auditory periphery model I/O

--- models/original/generate_ihcgram_BEZ2018_parallelized.m
+++ models/original/generate_ihcgram_BEZ2018_parallelized.m
@@ -1,15 +1,7 @@
 function vihc_mat = generate_ihcgram_BEZ2018_parallelized(stim,Fs_stim,species,numcfs,CFs)
 
-% Normal hearing
-ag_fs = [125 250 500 1e3 2e3 4e3 8e3];
-ag_dbloss = [0 0 0 0 0 0 0];                        
-
-% mixed loss
-dbloss = interp1(ag_fs,ag_dbloss,CFs,'linear','extrap');
-[cohcs,cihcs]=fitaudiogram2(CFs,dbloss,species);
-
-% cohcs  = ones(1,numcfs);  % normal ohc function
-% cihcs  = ones(1,numcfs);  % normal ihc function
+cohcs  = ones(1,numcfs);  % normal ohc function
+cihcs  = ones(1,numcfs);  % normal ihc function
 
 % stimulus parameters
 Fs = 100e3;  % sampling rate in Hz (must be 100, 200 or 500 kHz)
@@ -40,7 +32,3 @@ parfor CFind = 1:numcfs
 end
 
 vihc_mat = vihc_mat(:,1:round(T*Fs));
-
-% reduce sampling rate
-vihc_mat = resample(vihc_mat',Fs_stim,Fs)';
-
