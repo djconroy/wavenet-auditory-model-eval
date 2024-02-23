2c1ec4d614ebbdb70ece27606f96ec1482403526
Daniel Conroy <daniel.conroy@ucdconnect.ie>
2024-02-23 22:55:11 +0000

    refactoring and removing unnecessary repetitions

--- run_pipeline.m
+++ run_pipeline.m
@@ -18,34 +18,45 @@ function run_pipeline(spl, snr)
     cohcs  = ones(1, numCFs);  % normal ohc function
     cihcs  = ones(1, numCFs);  % normal ihc function
 
-    numsponts_healthy = [10 10 30]; % Number of low-spont, medium-spont, and high-spont fibers at each CF in a healthy AN
+    numsponts = [10 10 30]; % Number of low-spont, medium-spont, and high-spont fibers at each CF in a healthy AN
 
     if exist('ANpopulation.mat','file')
         load('ANpopulation.mat');
         disp('Loading existing population of AN fibers saved in ANpopulation.mat')
-        if (size(sponts.LS,2)<numsponts_healthy(1))||(size(sponts.MS,2)<numsponts_healthy(2))||(size(sponts.HS,2)<numsponts_healthy(3))||(size(sponts.HS,1)<numCFs||~exist('tabss','var'))
+        if (size(sponts.LS,2)<numsponts(1))||(size(sponts.MS,2)<numsponts(2))||(size(sponts.HS,2)<numsponts(3))||(size(sponts.HS,1)<numCFs||~exist('tabss','var'))
             disp('Saved population of AN fibers in ANpopulation.mat is too small - generating a new population');
-            [sponts,tabss,trels] = generateANpopulation(numCFs,numsponts_healthy);
+            [sponts,tabss,trels] = generateANpopulation(numCFs,numsponts);
         end
     else
-        [sponts,tabss,trels] = generateANpopulation(numCFs,numsponts_healthy);
+        [sponts,tabss,trels] = generateANpopulation(numCFs,numsponts);
         disp('Generating population of AN fibers, saved in ANpopulation.mat')
+    end
+
+    sponts_concat = zeros(numCFs,sum(numsponts));
+    tabss_concat = zeros(numCFs,sum(numsponts));
+    trels_concat = zeros(numCFs,sum(numsponts));
+
+    for CFlp = 1:numCFs
+        sponts_concat(CFlp,:) = [sponts.LS(CFlp,1:numsponts(1)) sponts.MS(CFlp,1:numsponts(2)) sponts.HS(CFlp,1:numsponts(3))];
+        tabss_concat(CFlp,:) = [tabss.LS(CFlp,1:numsponts(1)) tabss.MS(CFlp,1:numsponts(2)) tabss.HS(CFlp,1:numsponts(3))];
+        trels_concat(CFlp,:) = [trels.LS(CFlp,1:numsponts(1)) trels.MS(CFlp,1:numsponts(2)) trels.HS(CFlp,1:numsponts(3))];
     end
 
     implnt = 0;    % "0" for approximate or "1" for actual implementation of the power-law functions in the Synapse
     noiseType = 1;  % 0 for fixed fGn (1 for variable fGn)
     expliketype = 1; % 1 for shifted softplus (preferred); 0 for no expontential-like function; 2 for shifted exponential; 3 for shifted Boltmann
 
+    % stimulus parameters
+    Fs = 100e3;  % sampling rate in Hz (must be 100, 200 or 500 kHz)
+
     % PSTH parameters
     nrep = 1;
     psthbinwidth_mr = 100e-6; % mean-rate binwidth in seconds;
+    psthbins = round(psthbinwidth_mr*Fs);  % number of psth_ft bins per psth bin
     windur_ft=32;
     smw_ft = hamming(windur_ft);
     windur_mr=128;
     smw_mr = hamming(windur_mr);
-
-    % stimulus parameters
-    Fs = 100e3;  % sampling rate in Hz (must be 100, 200 or 500 kHz)
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%% END OF CODE NOT WRITTEN BY ME %%%%%%%%%%%%
@@ -97,31 +108,19 @@ function run_pipeline(spl, snr)
         vihc_mat=zeros(numCFs,length(vihc));
         neurogram_ft=zeros(numCFs,length(vihc));
         psth_ft = model_Synapse_BEZ2018a(vihc,CFs(1),nrep,1/Fs,noiseType,implnt,sponts.LS(1,1),tabss.LS(1,1),trels.LS(1,1),expliketype);
-        psthbins = round(psthbinwidth_mr*Fs);  % number of psth_ft bins per psth bin
         psth_mr = sum(reshape(psth_ft,psthbins,length(psth_ft)/psthbins));
         neurogram_mr=zeros(numCFs,length(psth_mr));
-        clear vihc psth_ft psthbins psth_mr
+        clear vihc psth_ft psth_mr
 
         parfor CFlp = 1:numCFs
             CF = CFs(CFlp);
-            cohc = cohcs(CFlp);
-            cihc = cihcs(CFlp);
-
-            numsponts = round([1 1 1].*numsponts_healthy); % Healthy AN
-            sponts_concat = [sponts.LS(CFlp,1:numsponts(1)) sponts.MS(CFlp,1:numsponts(2)) sponts.HS(CFlp,1:numsponts(3))];
-            tabss_concat = [tabss.LS(CFlp,1:numsponts(1)) tabss.MS(CFlp,1:numsponts(2)) tabss.HS(CFlp,1:numsponts(3))];
-            trels_concat = [trels.LS(CFlp,1:numsponts(1)) trels.MS(CFlp,1:numsponts(2)) trels.HS(CFlp,1:numsponts(3))];
-
-            vihc = model_IHC_BEZ2018a(stim100k,CF,nrep,1/Fs,simdur,cohc,cihc,species);
+            vihc = model_IHC_BEZ2018a(stim100k,CF,nrep,1/Fs,simdur,cohcs(CFlp),cihcs(CFlp),species);
             vihc_mat(CFlp,:) = vihc;
 
             for spontlp = 1:sum(numsponts)
-                spont = sponts_concat(spontlp);
-                tabs = tabss_concat(spontlp);
-                trel = trels_concat(spontlp);
-
-                psth_ft = model_Synapse_BEZ2018a(vihc,CF,nrep,1/Fs,noiseType,implnt,spont,tabs,trel,expliketype);
-                psthbins = round(psthbinwidth_mr*Fs);  % number of psth_ft bins per psth bin
+                psth_ft = model_Synapse_BEZ2018a(vihc,CF,nrep,1/Fs,noiseType,implnt,...
+                    sponts_concat(CFlp,spontlp),tabss_concat(CFlp,spontlp),...
+                    trels_concat(CFlp,spontlp),expliketype);
                 psth_mr = sum(reshape(psth_ft,psthbins,length(psth_ft)/psthbins));
 
                 neurogram_ft(CFlp,:) = neurogram_ft(CFlp,:)+filter(smw_ft,1,psth_ft);
