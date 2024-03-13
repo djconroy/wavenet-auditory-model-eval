969c40c72096846f1cd938d0e514c08cb817f92c
Daniel Conroy <daniel.conroy@ucdconnect.ie>
2024-03-13 20:09:49 +0000

    modify generate_ihcgram_BEZ2018_parallelized.m

--- models/original/generate_ihcgram_BEZ2018_parallelized.m
+++ models/original/generate_ihcgram_BEZ2018_parallelized.m
@@ -6,18 +6,11 @@ cihcs  = ones(1,numcfs);  % normal ihc function
 % stimulus parameters
 Fs = 100e3;  % sampling rate in Hz (must be 100, 200 or 500 kHz)
 stim100k = resample(stim,Fs,Fs_stim).';
-T  = length(stim100k)/Fs;  % stimulus duration in seconds
 
 % PSTH parameters
 nrep = 1;
-psthbinwidth_mr = 100e-6; % mean-rate binwidth in seconds;
 
-simdur = ceil(T*1.2/psthbinwidth_mr)*psthbinwidth_mr;
-
-% Run model_IHC_BEZ2018 function to estimate the size of vihc and variable.  
-vihc = model_IHC_BEZ2018a(stim100k,CFs(1),nrep,1/Fs,simdur,cohcs(1),cihcs(1),species);
-vihc_mat=zeros(numcfs,length(vihc));
-clear vihc
+vihc_mat=zeros(numcfs,length(stim100k));
 
 % loop over all CFs
 parfor CFind = 1:numcfs
@@ -26,9 +19,7 @@ parfor CFind = 1:numcfs
     cohc = cohcs(CFind);
     cihc = cihcs(CFind);
         
-    vihc = model_IHC_BEZ2018a(stim100k,CF,nrep,1/Fs,simdur,cohc,cihc,species);
+    vihc = model_IHC_BEZ2018a(stim100k,CF,nrep,1/Fs,length(stim100k),cohc,cihc,species);
     vihc_mat(CFind,:) = vihc;
     
 end
-
-vihc_mat = vihc_mat(:,1:round(T*Fs));
