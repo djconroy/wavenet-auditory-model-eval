eeb8ec52aca1245105be13c70efcb74f4a9c3505
Daniel Conroy <daniel.conroy@ucdconnect.ie>
2024-03-14 22:39:16 +0000

    implement asymmetric Gaussian filter for NSIM

--- nsim_calc.m
+++ nsim_calc.m
@@ -1,16 +1,23 @@
-% This script is a slightly modified version of code by Andrew Hines.
+% This script is a modified version of code by Andrew Hines.
 % This function calculates NSIM scores. NSIM is based on SSIM.
-% The only changes I made to the script:
-% 1. Create new variables specifying the window parameters
-% 2. Return NSIM scores for windows with specific CFs as well as the mean NSIM score
 
 function [freq_NSIMs, mNSIM] = nsim_calc(neuro_r, neuro_d)
-    % Window parameters
-    window_CFs = 3; % Must be an odd natural number
-    window_bins = 3; % Must be an odd natural number
-    gaussian_filter_std_dev = 0.5;
 
-    window = fspecial('gaussian', [window_CFs window_bins], gaussian_filter_std_dev);
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%% START OF CODE WRITTEN BY ME %%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    % Approach to implementing asymmetric Gaussian filter
+    % taken from https://stackoverflow.com/a/15241151
+    vertical = fspecial('gaussian', [5 1], 1);
+    horizontal = fspecial('gaussian', [1 3], 0.5);
+    window = vertical * horizontal;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%% START OF CODE NOT WRITTEN BY ME %%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    %window = fspecial('gaussian', [3 3], 0.5);
     window = window/sum(sum(window));
     %dynamic range set to max of reference neuro
     L=max(max(neuro_r));
@@ -33,6 +40,12 @@ function [freq_NSIMs, mNSIM] = nsim_calc(neuro_r, neuro_d)
     sigma_d=sign(sigma_d_sq).*sqrt(abs(sigma_d_sq));
     L_r_d= (2*mu_r.*mu_d+C1) ./(mu_r_sq+mu_d_sq +C1);
     S_r_d= (sigma_r_d + C2)./(sigma_r.*sigma_d +C2);
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%% START OF CODE WRITTEN BY ME %%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    % Return NSIM scores for windows with specific CFs as well as the mean NSIM score
     freq_NSIMs = mean(L_r_d.*S_r_d, 2);
     mNSIM = mean(freq_NSIMs);
 end
