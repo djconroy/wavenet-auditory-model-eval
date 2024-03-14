6e95947381d77676cf825ff9be1ecbef8abf3c39
Daniel Conroy <daniel.conroy@ucdconnect.ie>
2024-03-14 23:02:25 +0000

    specify all arguments to the general pipeline

--- run_pipeline.m
+++ run_pipeline.m
@@ -1,6 +1,8 @@
-% Change audio_file_name as you wish and then run the pipeline from the terminal with
+% Change the input arguments as you wish and then run the pipeline from the terminal with
 % matlab -batch "run_pipeline"
 
 audio_file_name = "";
+spl = 60;
+snr = "N/A";
 
-gen_comparison_metrics(audio_file_name);
+gen_comparison_metrics(audio_file_name, spl, snr);
