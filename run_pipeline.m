0c69806326ce291afd672e9e80d9bb66bef6054a
Daniel Conroy <daniel.conroy@ucdconnect.ie>
2024-03-14 22:40:51 +0000

    add script to run general pipeline

---
+++ run_pipeline.m
@@ -0,0 +1,6 @@
+% Change audio_file_name as you wish and then run the pipeline from the terminal with
+% matlab -batch "run_pipeline"
+
+audio_file_name = "";
+
+gen_comparison_metrics(audio_file_name);
