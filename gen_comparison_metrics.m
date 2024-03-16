6c94a51b3ca1606f7ac69acf017e31372c840159
Daniel Conroy <daniel.conroy@ucdconnect.ie>
2024-03-16 00:13:53 +0000

    move neurogram generation to a separate function

--- gen_comparison_metrics.m
+++ gen_comparison_metrics.m
@@ -116,52 +116,15 @@ function gen_comparison_metrics(audio_file_name, spl, snr)
         sponts_concat(1, 1), tabss_concat(1, 1), trels_concat(1, 1), expliketype);
     clear vihcFs_wavenet
 
-
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%% START OF CODE WRITTEN BY ANDREW HINES %%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-    psthbinwidth_env = 100e-6;
-    env_windowsize = 128;
-    env_overlap = 64;
-    w_env = hamming(env_windowsize, 'periodic');
-
-    psthbinwidth_tfs = 10e-6;
-    tfs_windowsize = 32;
-    tfs_overlap = 16;
-    w_tfs = hamming(tfs_windowsize, 'periodic');
-
-    psthbins_env = round(psthbinwidth_env * Fs);
-
-    psth_env_wavenet = psth_tfs_wavenet(1:psthbins_env * floor(length(psth_tfs_wavenet) / psthbins_env));
-    pr_env = sum(reshape(psth_env_wavenet, psthbins_env, length(psth_env_wavenet) / psthbins_env)); % pr of spike in each bin
-    psth_env_wavenet = pr_env / psthbinwidth_env; % psth in units of spikes/s
-    clear pr_env
-    b_sp_env = spectrogram(psth_env_wavenet, w_env, env_overlap, env_windowsize, 1 / psthbinwidth_env);
-    clear psth_env_wavenet
-    synrate_env = abs(b_sp_env / env_windowsize / sqrt(sum(w_env.^2) / length(w_env)));
-    clear b_sp_env
-    cf_env_neurogram = synrate_env(1, :); % get the dc value
-
-    psth_tfs_wavenet = psth_tfs_wavenet / psthbinwidth_tfs; % psth in units of spikes/s
-    b_sp_tfs = spectrogram(psth_tfs_wavenet, w_tfs, tfs_overlap, tfs_windowsize, 1 / psthbinwidth_tfs);
-    clear psth_tfs_wavenet
-    synrate_tfs = abs(b_sp_tfs / tfs_windowsize / sqrt(sum(w_tfs.^2) / length(w_tfs)));
-    clear b_sp_tfs
-    cf_tfs_neurogram = synrate_tfs(1, :); % get the dc value
-
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%% START OF CODE WRITTEN BY ME %%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+    [env_neurogram_row, tfs_neurogram_row] = gen_neurogram_row(psth_tfs_wavenet);
 
     % Memory allocation for neurograms
-    env_neurogram = zeros(numCFs, length(cf_env_neurogram));
-    env_neurogram_wavenet = zeros(numCFs, length(cf_env_neurogram));
-    clear synrate_env cf_env_neurogram
-    tfs_neurogram = zeros(numCFs, length(cf_tfs_neurogram));
-    tfs_neurogram_wavenet = zeros(numCFs, length(cf_tfs_neurogram));
-    clear synrate_tfs cf_tfs_neurogram
+    env_neurogram = zeros(numCFs, length(env_neurogram_row));
+    env_neurogram_wavenet = zeros(numCFs, length(env_neurogram_row));
+    clear env_neurogram_row
+    tfs_neurogram = zeros(numCFs, length(tfs_neurogram_row));
+    tfs_neurogram_wavenet = zeros(numCFs, length(tfs_neurogram_row));
+    clear tfs_neurogram_row
 
     % Memory allocation for SDRs
     SDRs = zeros(1, numCFs);
@@ -201,33 +164,9 @@ function gen_comparison_metrics(audio_file_name, spl, snr)
         
         clear vihcFs
 
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %%%%%%% START OF CODE WRITTEN BY ANDREW HINES %%%%%%%
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-        psth_env = psth_tfs(1:psthbins_env * floor(length(psth_tfs) / psthbins_env));
-        pr_env = sum(reshape(psth_env, psthbins_env, length(psth_env) / psthbins_env)); % pr of spike in each bin
-        psth_env = pr_env / psthbinwidth_env; % psth in units of spikes/s
-        clear pr_env
-        b_sp_env = spectrogram(psth_env, w_env, env_overlap, env_windowsize, 1 / psthbinwidth_env);
-        clear psth_env
-        synrate_env = abs(b_sp_env / env_windowsize / sqrt(sum(w_env.^2) / length(w_env)));
-        clear b_sp_env
-        cf_env_neurogram = synrate_env(1, :); % get the dc value
-        env_neurogram(CFnum, :) = cf_env_neurogram;
-
-        psth_tfs = psth_tfs / psthbinwidth_tfs; % psth in units of spikes/s
-        b_sp_tfs = spectrogram(psth_tfs, w_tfs, tfs_overlap, tfs_windowsize, 1 / psthbinwidth_tfs);
-        clear psth_tfs
-        synrate_tfs = abs(b_sp_tfs / tfs_windowsize / sqrt(sum(w_tfs.^2) / length(w_tfs)));
-        clear b_sp_tfs
-        cf_tfs_neurogram = synrate_tfs(1, :); % get the dc value
-        tfs_neurogram(CFnum, :) = cf_tfs_neurogram;
-
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %%%%%%%%%%%% START OF CODE WRITTEN BY ME %%%%%%%%%%%%
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+        [env_neurogram_row, tfs_neurogram_row] = gen_neurogram_row(psth_tfs);
+        env_neurogram(CFnum, :) = env_neurogram_row;
+        tfs_neurogram(CFnum, :) = tfs_neurogram_row;
 
         psth_tfs_wavenet = zeros(1, vihcFs_wavenet_len);
 
@@ -238,39 +177,13 @@ function gen_comparison_metrics(audio_file_name, spl, snr)
             psth_tfs_wavenet = psth_tfs_wavenet + single_psth_tfs;
             % psth_tfs_wavenet is a reduction variable in this parfor loop
         end
-
         clear vihcFs_wavenet
-
         % Average the PSTH for all the simulated auditory nerves
         psth_tfs_wavenet = psth_tfs_wavenet / sum(numsponts);
 
-
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %%%%%%% START OF CODE WRITTEN BY ANDREW HINES %%%%%%%
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-        psth_env_wavenet = psth_tfs_wavenet(1:psthbins_env * floor(length(psth_tfs_wavenet) / psthbins_env));
-        pr_env = sum(reshape(psth_env_wavenet, psthbins_env, length(psth_env_wavenet) / psthbins_env)); % pr of spike in each bin
-        psth_env_wavenet = pr_env / psthbinwidth_env; % psth in units of spikes/s
-        clear pr_env
-        b_sp_env = spectrogram(psth_env_wavenet, w_env, env_overlap, env_windowsize, 1 / psthbinwidth_env);
-        clear psth_env_wavenet
-        synrate_env = abs(b_sp_env / env_windowsize / sqrt(sum(w_env.^2) / length(w_env)));
-        clear b_sp_env
-        cf_env_neurogram = synrate_env(1, :); % get the dc value
-        env_neurogram_wavenet(CFnum, :) = cf_env_neurogram;
-
-        psth_tfs_wavenet = psth_tfs_wavenet / psthbinwidth_tfs; % psth in units of spikes/s
-        b_sp_tfs = spectrogram(psth_tfs_wavenet, w_tfs, tfs_overlap, tfs_windowsize, 1 / psthbinwidth_tfs);
-        clear psth_tfs_wavenet
-        synrate_tfs = abs(b_sp_tfs / tfs_windowsize / sqrt(sum(w_tfs.^2) / length(w_tfs)));
-        clear b_sp_tfs
-        cf_tfs_neurogram = synrate_tfs(1, :); % get the dc value
-        tfs_neurogram_wavenet(CFnum, :) = cf_tfs_neurogram;
-
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %%%%%%%%%%%% START OF CODE WRITTEN BY ME %%%%%%%%%%%%
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        [env_neurogram_row, tfs_neurogram_row] = gen_neurogram_row(psth_tfs_wavenet);
+        env_neurogram_wavenet(CFnum, :) = env_neurogram_row;
+        tfs_neurogram_wavenet(CFnum, :) = tfs_neurogram_row;
     end
 
     clear stimFs ihcogram
---
+++ gen_neurogram_row.m
@@ -0,0 +1,34 @@
+function [env_neurogram_row, tfs_neurogram_row] = gen_neurogram_row(psth_tfs)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%% CODE WRITTEN BY ANDREW HINES %%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    psthbinwidth_env = 100e-6;
+    env_windowsize = 128;
+    env_overlap = 64;
+    w_env = hamming(env_windowsize, 'periodic');
+
+    psthbinwidth_tfs = 10e-6;
+    tfs_windowsize = 32;
+    tfs_overlap = 16;
+    w_tfs = hamming(tfs_windowsize, 'periodic');
+
+    Fs = 100e3;
+    psthbins_env = round(psthbinwidth_env * Fs);
+
+    psth_env = psth_tfs(1:psthbins_env * floor(length(psth_tfs) / psthbins_env));
+    pr_env = sum(reshape(psth_env, psthbins_env, length(psth_env) / psthbins_env)); % pr of spike in each bin
+    psth_env = pr_env / psthbinwidth_env; % psth in units of spikes/s
+    clear pr_env
+    b_sp_env = spectrogram(psth_env, w_env, env_overlap, env_windowsize, 1 / psthbinwidth_env);
+    clear psth_env
+    synrate_env = abs(b_sp_env / env_windowsize / sqrt(sum(w_env.^2) / length(w_env)));
+    clear b_sp_env
+    env_neurogram_row = synrate_env(1, :); % get the dc value
+
+    psth_tfs = psth_tfs / psthbinwidth_tfs; % psth in units of spikes/s
+    b_sp_tfs = spectrogram(psth_tfs, w_tfs, tfs_overlap, tfs_windowsize, 1 / psthbinwidth_tfs);
+    synrate_tfs = abs(b_sp_tfs / tfs_windowsize / sqrt(sum(w_tfs.^2) / length(w_tfs)));
+    clear b_sp_tfs
+    tfs_neurogram_row = synrate_tfs(1, :); % get the dc value
+end
