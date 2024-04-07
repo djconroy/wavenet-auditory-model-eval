255544e38215de205591df52dbead7d2e298a358
Daniel Conroy <daniel.conroy@ucdconnect.ie>
2024-04-07 17:07:05 +0100

    rename dialect variable and use format strings

--- extract_subsets_of_timit.py
+++ extract_subsets_of_timit.py
@@ -11,8 +11,8 @@ TIMIT_CORE_TEST_SET_SPEAKERS = {
 }
 TIMIT_CORE_TEST_SET_DIRS = {
     key: [
-        TIMIT_CORE_TEST_SET_PREFIX + str(dialect) + "/" + key[0] + speaker
-        for dialect, speaker in enumerate(speakers, start=1)
+        f"{TIMIT_CORE_TEST_SET_PREFIX}{dialect_region}/{key[0]}{speaker}"
+        for dialect_region, speaker in enumerate(speakers, start=1)
     ]
     for key, speakers in TIMIT_CORE_TEST_SET_SPEAKERS.items()
 }
@@ -21,18 +21,18 @@ for key, core_test_set in TIMIT_CORE_TEST_SET_DIRS.items():
 
     core_test_set_dir = TIMIT_DATASETS_DIR + TIMIT_CORE_TEST_SET + key
 
-    for dialect, speaker in enumerate(TIMIT_CORE_TEST_SET_SPEAKERS[key], start=1):
+    for dialect_region, speaker in enumerate(TIMIT_CORE_TEST_SET_SPEAKERS[key], start=1):
 
         # Create a directory to store the test data files for the speaker
-        core_test_set_speaker_dir = core_test_set_dir + "/DR" + str(dialect) + "/" + speaker
+        core_test_set_speaker_dir = f"{core_test_set_dir}/DR{dialect_region}/{speaker}"
         os.makedirs(core_test_set_speaker_dir, exist_ok=True)
 
         # Get the test data files for the speaker
-        timit_data = os.listdir(core_test_set[dialect - 1])
+        timit_data = os.listdir(core_test_set[dialect_region - 1])
         # Exclude the SA sentence data files
         timit_data = [file for file in timit_data if not file.startswith("SA")]
 
         # Copy the test data files for the speaker into the newly-created directory
         for file in timit_data:
-            #shutil.move(core_test_set[dialect - 1] + "/" + file, core_test_set_speaker_dir)
-            shutil.copy(core_test_set[dialect - 1] + "/" + file, core_test_set_speaker_dir)
+            #shutil.move(f"{core_test_set[dialect_region - 1]}/{file}", core_test_set_speaker_dir)
+            shutil.copy(f"{core_test_set[dialect_region - 1]}/{file}", core_test_set_speaker_dir)
